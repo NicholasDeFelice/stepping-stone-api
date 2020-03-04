699e352c718f75ffbf1f376111395990ed85556a
Michael Barbas <michaelbarbas@gmail.com>
2020-03-03 22:36:45 -0500

    initial commit | firebase

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,6 @@ const functions = require('firebase-functions');
 const app = require('./express/app');
 const express = require('express');
 
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -10,8 +9,8 @@ const express = require('express');
 //  response.send("Hello from Firebase!");
 // });
 
-const expressServer = express();
+const expServer = express();
 
-expressServer.use('/api', app);
+expServer.use('/api', app);
 
-exports.api = functions.https.onRequest(expressServer);
+exports.api = functions.https.onRequest(expServer);
